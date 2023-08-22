3f7933b824930d2b1a4b878537f37df2414803d4
JT Smith <nexxai@gmail.com>
2023-08-22 14:25:47 -0600

    Implemented without.js

---
+++ without.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function without(source, itemsToRemove) {
+  var newArray = [];
+
+  for (i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+
+  return newArray;
+}
+
+assertArraysEqual(without([1, 2, 3], [1]), [2,3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ['1', '2']);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
