bec6c6af9f3e45c9b8b1ad7f3b20a35877b08ff8
JT Smith <nexxai@gmail.com>
2023-08-23 14:47:32 -0600

    Implemented letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  for (i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+
+  return results;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+assertArraysEqual(letterPositions("hello").h, [0])
+assertArraysEqual(letterPositions("hello").e, [1])
+assertArraysEqual(letterPositions("hello").l, [2, 3])
+assertArraysEqual(letterPositions("hello").o, [4])
