9bca7bb70d3b99281fadc29f67c452eb957a617f
JT Smith <nexxai@gmail.com>
2023-08-23 14:21:21 -0600

    Implementing middle.js

---
+++ middle.js
@@ -0,0 +1,42 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function middle(arr) {
+  if (arr.length <= 2) {
+    return [];
+  } else if (arr.length % 2 == 1) {
+    let middleValueIndex = arr.length / 2 - 0.5;
+
+    return [arr[middleValueIndex]];
+  } else {
+    let middleValueIndex = arr.length / 2;
+
+    return [arr[middleValueIndex - 1], arr[middleValueIndex]];
+  }
+}
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
