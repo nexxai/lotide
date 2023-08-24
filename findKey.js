2f99414787b7dc2b6a2a63b316f12d2354a1eb16
JT Smith <nexxai@gmail.com>
2023-08-24 14:58:54 -0600

    Implemented findKey function

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKey(obj, callback) {
+  for (key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
