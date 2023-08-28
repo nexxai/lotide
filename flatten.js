89790f0e786a443436e7c436e400ead24109b22d
JT Smith <nexxai@gmail.com>
2023-08-28 15:53:46 -0600

    Refactored tests out of Flatten function

--- flatten.js
+++ flatten.js
@@ -1,26 +1,4 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function flatten(arr) {
+const flatten = function(arr) {
   let newArray = [];
 
   arr.forEach((item) => {
@@ -36,4 +14,4 @@ function flatten(arr) {
   return newArray;
 }
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+module.exports = flatten;
