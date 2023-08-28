5deb6700ba85f20ceb6efc81f9ef35244577896c
JT Smith <nexxai@gmail.com>
2023-08-28 13:09:28 -0600

    Refactored tests out of tail

--- tail.js
+++ tail.js
@@ -1,16 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
