9ba77b711b1e058dd9a4f03ce71774af9c43a04e
JT Smith <nexxai@gmail.com>
2023-08-28 13:18:41 -0600

    Refactored tests out of middle

--- middle.js
+++ middle.js
@@ -1,26 +1,4 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function middle(arr) {
+const middle = function(arr) {
   if (arr.length <= 2) {
     return [];
   } else if (arr.length % 2 == 1) {
@@ -34,9 +12,4 @@ function middle(arr) {
   }
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
